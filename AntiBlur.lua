b5cafbc6cc019fe3280d8f4fc0991a70eebaab24
MrSpenk <16985893+MrSpenk@users.noreply.github.com>
2018-06-18 03:48:36 +0400

    Add files via upload

---
+++ AntiBlur.lua
@@ -0,0 +1,63 @@
+local AntiBlur = {}
+
+AntiBlur.optionEnable = Menu.AddOptionBool({ "Awareness", "Anti-Blur PA" }, "Activation", false)
+AntiBlur.optionSize = Menu.AddOptionSlider({ "Awareness", "Anti-Blur PA" }, "Size", 800, 2000, 1000)
+
+local timer = 0
+local Phantom = {false, false, false}
+
+function AntiBlur.GetPA(object)
+	if not object[1] then
+		for i = 1, 10 do
+			local hero = Heroes.Get(i)
+			if hero and Entity.IsEntity( hero ) then
+				local name = NPC.GetUnitName( hero )
+				if (name == "npc_dota_hero_phantom_assassin") then
+					object[1] = hero
+					return
+				end
+			end
+		end
+	end
+end
+
+function AntiBlur.FindPA(object)
+	local visible = not Entity.IsDormant( object[1] )
+	local hasBlur = NPC.HasModifier(object[1], "modifier_phantom_assassin_blur_active") or false
+	
+	if visible and hasBlur then
+		local pos = Entity.GetAbsOrigin( object[1] )
+		object[2] = pos
+		object[3] = true
+	else
+		object[2] = false
+		object[3] = false
+	end
+
+	timer = os.clock() + 1
+end
+
+function AntiBlur.OnUpdate()
+	AntiBlur.enabled = Menu.IsEnabled( AntiBlur.optionEnable )
+	if not AntiBlur.enabled then return end
+
+	if not Phantom[1] then
+		AntiBlur.GetPA(Phantom)
+	end
+	
+	AntiBlur.Size = Menu.GetValue( AntiBlur.optionSize )
+	
+	if timer - os.clock() <= 0 then
+		AntiBlur.FindPA(Phantom)
+	end
+end
+
+function AntiBlur.OnDraw()
+	if not AntiBlur.enabled then return end
+
+	if Phantom[3] and Phantom[2] then
+		MiniMap.DrawHeroIcon("npc_dota_hero_phantom_assassin", Phantom[2], 255, 255, 255, 255, AntiBlur.Size )
+	end
+end
+
+return AntiBlur
